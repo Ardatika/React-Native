0003872e995cb5dc399eccc52b275d2be2077560
Ardatika <ardatikadc@gmail.com>
2023-10-04 10:07:00 +0700

    Ini Tugas Modul 1 Ardatika

---
+++ tugas/counter.js
@@ -0,0 +1,28 @@
+//Class Component
+const { Component } = React;
+const { render } = ReactDOM;
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+        };
+    }
+    increment = () => {
+        this.setState({ count: this.state.count + 1 });
+    };
+    decrement = () => {
+        this.setState({ count: this.state.count - 1 });
+    };
+    render() {
+        return (
+            <div>
+                <h1>{this.state.count.toString().padStart('0')}</h1>
+                <button onClick={this.decrement}>-1</button>
+                <button onClick={this.increment}>+1</button>
+            </div>
+        );
+    }
+}
+render(<Counter />, document.getElementById('root'));
